9164dd758edac40ada2ce80f9e51e82d90de2faa
BilalyC <bilaly.camara@outlook.com>
2018-12-17 17:15:24 +0100

    Exercice 8 Complet

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez donner un rayon r : ");
+            int r = int.Parse(Console.ReadLine());
+            Console.WriteLine("Le périmètre est : " + (Math.PI * (2 * r)));
+            Console.WriteLine("La surface est : " + (Math.PI * Math.Pow(r, 2)));
+        }
+    }
+}
